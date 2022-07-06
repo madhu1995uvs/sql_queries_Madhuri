499bf43e53c94cf465ba070fc1c1a263ae9c29ef
madhu1995uvs <103286899+madhu1995uvs@users.noreply.github.com>
2022-07-06 12:04:48 -0400

    Added UTI Query to repository

---
+++ UTI Query.sql
@@ -0,0 +1,154 @@
+
+/*
+, case
+		when result_ua like '%>%'
+		then 'result greater than number listed'
+		when result_ua like '%<%'
+		then 'result less than number listed'
+		when result like '%-%'
+		then 'lower number from range used'
+		when result like '%+%'
+		then 'result with +'
+			
+	end result_ua_modifiers
+	*/
+
+	USE EMTCQIData
+
+DECLARE @Start Date
+DECLARE @End Date
+
+SET @Start = '01/01/18'
+SET @End = '04/01/21';
+	
+	
+	-- 	add cte for udip le and nitrite and categorize udip and ua
+
+with ua_results as
+	(
+			select distinct
+			pt_fin
+			,RESULT_MNEMONIC as urine_result_ua
+			,result as result_ua
+			--,try_convert(integer,result) as numeric_result
+			from LAB_DATA_IMPORT_Master
+			WHERE (ORDER_MNEMONIC like 'complete%') 
+			and (RESULT_MNEMONIC like '%Urine Leukocyte Esterase'
+		or RESULT_MNEMONIC like '%urine wbc' or RESULT_MNEMONIC like '%urine nitrite') 
+		and (result like '%[0-9]%' 
+		and (result not like '%[a-z]%')
+			or (result like '%large%' or result like '%moderate%' or result like '%small%' or result like '%ref range%'))
+			)
+			
+
+
+, EDTAT as
+	(
+	select distinct 
+	PATIENT_FIN
+	,PATIENT_MRN
+	, PT_GENDER
+	,PT_RACE
+	,PT_ETHNICITY
+	,datediff(month,PT_DOB, CHECKIN_DATE_TIME) age_months
+	,FIRST_MD_SEEN
+	,LAST_ASSIGNED_MD
+	,REASON_FOR_VISIT
+	,PT_DX1
+	,PT_DX2
+	,PT_DX3
+	, left(tat.PT_ACUITY,1) ESI
+	,bill.language
+	, bill.nachri_types
+	,dispo_date_time
+	,CHECKIN_DATE_TIME
+
+	from ED_TAT_MASTER tat
+
+
+		INNER JOIN 
+			(
+			select distinct patient_identification, language, nachri_types, 
+			concat (',',LTRIM(ICD1),',',LTRIM(ICD2),',',ltrim(ICD3),',',ltrim(ICD4),',',ltrim(ICD5),',',ltrim(ICD6),',',ltrim(ICD7),',',ltrim(ICD8),',',ltrim(ICD9),','
+			,ltrim(ICD10),',',ltrim(ICD11),',',ltrim(ICD12),',',ltrim(ICD13),',',ltrim(ICD14),',',ltrim(ICD15)) ALLICD
+			from ED_BILLING_DATA_Master
+			)
+		BILL on BILL.PATIENT_IDENTIFICATION = tat.PATIENT_FIN )
+
+
+
+ 
+, tat_bb as (
+       select bb =1
+       , patient_fin as patient_fin_bb
+       , PATIENT_MRN as patient_mrn_bb
+       , CHECKIN_DATE_TIME as checkin_date_time_bb
+       from ED_TAT_MASTER
+       )
+ 
+
+,urine_culture as 
+(
+	select distinct 
+PT_FIN
+, order_mnemonic, result, case	when result like '%cathet%'	then '1'	else '0'end urine_cath		, case 			when result like '%[5-9]0,000%'			or result like '%00,000%'			then '1'			else '0'		end positive_culture
+
+from LAB_DATA_IMPORT_Master
+
+	where RESULT_DT_TM between @Start and @end	and ORDER_MNEMONIC like '%urine culture%')
+
+
+	, notes as (
+       select pt_fin
+       , RESULT_TITLE_TEXT
+       , RESULT_DT_TM
+       , SUBSTRING(result,charindex('allergies (active)',result),1000) as allergy_str
+       from ED_NOTES_MASTER
+       where result like '%allergies (active%'
+       and RESULT_DT_TM between @Start and @end
+       )
+ 
+, small_notes as (
+       select *
+       , case
+              when allergy_str like '%Medication list%'
+              then left(allergy_str,charindex('Medication list',allergy_str)-1)
+              else allergy_str
+       end allergy_substr
+       from notes
+       )
+
+
+
+
+
+select * from EDTAT
+
+INNER JOIN
+	ua_results ON ua_results.PT_FIN = edtat.PATIENT_FIN
+
+left outer join 
+       (select pt_fin
+       , FIRST_VALUE(allergy_substr) over (partition by pt_fin order by result_dt_tm asc rows unbounded preceding) as first_allergy
+       from small_notes
+       )
+       overall_allergy on EDTAT.patient_fin=overall_allergy.pt_fin
+
+
+left outer join 
+	urine_culture
+	on ua_results.PT_FIN = urine_culture.PT_FIN
+
+left outer join
+       (select *
+       from tat_bb) 
+       bounceback on EDTAT.patient_mrn = bounceback.patient_mrn_bb
+       and datediff(Hour,EDTAT.dispo_date_time,bounceback.checkin_date_time_bb)>8
+       and datediff(Hour,EDTAT.dispo_date_time,bounceback.checkin_date_time_bb)<72
+ 
+where EDTAT.CHECKIN_DATE_TIME between @start and @end
+
+
+
+
+	
