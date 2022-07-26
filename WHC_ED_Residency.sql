3be0bd8a2d0e89dc851de8cc63fe3aa364c5ca14
kennymckinley <56144157+kennymckinley@users.noreply.github.com>
2022-07-26 01:01:01 -0400

    mostly_complete_residency_admin_query

--- WHC_ED_Residency.sql
+++ WHC_ED_Residency.sql
@@ -6,12 +6,30 @@ DECLARE @End Date
 SET @Start = '07/01/18'
 SET @End = '07/01/22'
 
-; with tat as (
+; with notes as (
+	select distinct PT_FIN
+	, case when result like '%critical%' then '1' end destination_icu
+	, case when result like '%OR (Operating Room)%' then '1' end destination_or
+	from ED_NOTES_MASTER
+	where RESULT_TITLE_TEXT like '%ed bed%'
+	and RESULT_DT_TM between @start and @end
+	and (result like '%critical%' or result like '%Destination Requested :   OR (Operating Room)%')
+	and result not like '%Care Team :   Psychiatry%'
+	)
+
+
+, tat as (
 	select distinct 
 	PATIENT_FIN
 	,PATIENT_MRN
+	, destination_icu
+	, destination_or
 	,datediff(YEAR,PT_DOB, CHECKIN_DATE_TIME) age_years
+	, datepart(hour, checkin_date_time) as hov
+	, datepart(DAYOFYEAR, checkin_date_time) as dov
 	,DATEPART(year,checkin_date_time) as yov
+	, datepart(WEEKDAY, checkin_date_time) as weekday_number
+	, datename(weekday, checkin_date_time) as weekday_name
 	,FIRST_MD_SEEN
 	,LAST_ASSIGNED_MD
 	,REASON_FOR_VISIT
@@ -30,6 +48,25 @@ SET @End = '07/01/22'
 	, case  when pt_disch_dispo like 'DISCHARGE' or PT_DISCH_DISPO like '%home%' then '1' end discharge
 	, case WHEN PT_DISCH_DISPO LIKE 'Expired%' or pt_disch_dispo like 'died%' or pt_disch_dispo like 'dead%' THEN '1' end deceased
 	, case when datediff(YEAR,PT_DOB, CHECKIN_DATE_TIME) < 18 then '1' end under_18y
+	, case when PT_ACUITY like '%4%' or pt_acuity like '%5%' then '1' end low_acuity
+	
+	, case when FIRST_RESIDENT_SEEN is null  /*patients seen by EM faculty*/
+		and PT_ACUITY like '%1%' or pt_acuity like '%2%' or pt_acuity like '%3%'
+		then '1'
+		end seen_by_EM_Faculty
+
+	, case when FIRST_RESIDENT_SEEN is null  /*patients seen by non EM faculty*/
+		and PT_ACUITY like '%4%' or pt_acuity like '%5%'
+		and First_MD_seen not like '%PA-C' 
+		and First_MD_seen not like '%NP' 
+		then '1'
+		end seen_by_non_EM_Faculty
+
+	, case when FIRST_RESIDENT_SEEN not like '%PA-C' /*substract from patients seen by EM residents*/
+		and First_RESIDENT_seen not like '%NP' 
+		then '1' 
+		end seen_by_non_EM_Residents
+
 	, case when FIRST_RESIDENT_SEEN like '%ierardo, aly%' 
 		or FIRST_RESIDENT_SEEN like '%johnson, matthew%'
 		or FIRST_RESIDENT_SEEN like '%migdal, talia%'
@@ -41,43 +78,97 @@ SET @End = '07/01/22'
 		or FIRST_RESIDENT_SEEN like '%Bracy, Connor%'
 		or FIRST_RESIDENT_SEEN like '%Castillo, Glennette%'
 		or FIRST_RESIDENT_SEEN like '%Chan, Virginia%'
-		or FIRST_RESIDENT_SEEN like '%Harrington, Emily%'
+		or FIRST_RESIDENT_SEEN like '%Emily, Harrington%'
 		or FIRST_RESIDENT_SEEN like '%Lew, Kelly%'
 		or FIRST_RESIDENT_SEEN like '%Peragine, Brian%'
 		or FIRST_RESIDENT_SEEN like '%Sheng, Bowen%'
-		or FIRST_RESIDENT_SEEN like '%Nam, Trinh%'
+		or FIRST_RESIDENT_SEEN like '%Trinh, Nam%'
 		or FIRST_RESIDENT_SEEN like '%Weisbein, Alexa%'
 		or FIRST_RESIDENT_SEEN like '%Williams, Brandon%'
 		then '1' 
 		end seen_by_em_res
-	
+
+	, case when FIRST_MD_SEEN like '%NP'
+		or FIRST_MD_SEEN like '%PA'
+		or FIRST_MD_SEEN like '%PA-C'
+	then '1' 
+		end seen_by_PA_NP
+
 	from ED_TAT_MASTER tat
+	left outer join notes
+	on tat.PATIENT_FIN=notes.PT_FIN
 
 	where (CHECKIN_DATE_TIME between @start and @end) and (TRACK_GROUP like '%ED Track%')
 	and PT_ACUITY is not null
 	)
 
-/*
---KWM: I think using notes is less accurate than using pt_disch_dispo for death
-, notes as (
-       select distinct pt_fin
-	   , death_note = 1
-       , RESULT_TITLE_TEXT
-       , RESULT_DT_TM
-       from ED_NOTES_MASTER
-	   where RESULT_TITLE_TEXT like '%death%'
-       and RESULT_DT_TM between @Start and @end
-       )
-*/
+, daily_phys_hrs as (
+	select *
+	, case when weekday_number=1 or weekday_number=7 then '1' else '0' end weekend
+	from tat
+	where FIRST_MD_SEEN not like '%np'
+	and first_md_seen not like '%pa-c'
+	)
 
-select academic_year
-, count(PATIENT_FIN) as total_ed_patients
-, count(admission) as admitted_from_ed
-, count(under_18y) as pediatric_0_to_17
-, count(discharge) as discharged_patients
-, count(deceased) as deaths_in_ed
---, count(death_note) as deaths_by_notes
-, count(seen_by_em_res) as patients_seen_by_em_res
-from tat
---left outer join notes on tat.patient_fin =  notes.pt_fin
-group by academic_year
+, provider_wkday as (
+	select hov, dov, academic_year
+	, count(distinct first_md_seen) as unique_providers_wkday
+	from daily_phys_hrs
+	where weekend=0
+	group by hov, dov, academic_year
+	)
+
+, provider_wkend as (
+	select hov, dov, academic_year
+	, count(distinct first_md_seen) as unique_providers_wkend
+	from daily_phys_hrs
+	where weekend=1
+	group by hov, dov, academic_year
+	)
+
+, provider_wkday_sum as (
+	select academic_year
+	, sum(unique_providers_wkday) as wkday_prov_hrs
+	from provider_wkday
+	group by academic_year
+	)
+
+, provider_wkend_sum as (
+	select academic_year as academic_year1
+	, sum(unique_providers_wkend) as wkend_prov_hrs
+	from provider_wkend
+	group by academic_year
+	)
+
+, provider_hr_avg as (
+	select *
+	, wkday_prov_hrs/261 as avg_wkday_prov_hrs --average year with 365 days total, 5/7 are weekdays
+	, wkend_prov_hrs/104 as avg_wkend_prov_hrs
+	from provider_wkday_sum	
+	inner join provider_wkend_sum on provider_wkday_sum.academic_year=provider_wkend_sum.academic_year1
+	)
+
+, patient_counts as (
+	select academic_year
+	, count(PATIENT_FIN) as total_ed_patients
+	, count(admission) as admitted_from_ed
+	, count (destination_icu) as admits_to_icu
+	, count (destination_or) as dispo_to_or
+	, count(under_18y) as pediatric_0_to_17
+	, count(low_acuity) as fast_track_possible
+	, count(discharge) as discharged_patients
+	, count(deceased) as deaths_in_ed
+	, count(seen_by_em_res) as patients_seen_by_em_res
+	, count (seen_by_PA_NP) as patients_seen_by_PA_N
+	, count (seen_by_EM_Faculty) as patients_seen_by_EM_Faculty
+	, count (seen_by_non_EM_Faculty) as patients_seen_by_non_EM_Faculty
+	, count (seen_by_non_EM_Residents) as patients_seen_by_non_EM_Residents
+
+	from tat
+	group by academic_year
+	)
+
+select * 
+from patient_counts
+inner join provider_hr_avg 
+on patient_counts.academic_year=provider_hr_avg.academic_year1
