c7b24fdf00f3804443fdb256546ee479e751e3cb
kennymckinley <56144157+kennymckinley@users.noreply.github.com>
2022-08-08 20:24:38 -0400

    simplified cte

--- ucx_source.sql
+++ ucx_source.sql
@@ -6,26 +6,20 @@ DECLARE @End Date
 SET @Start = '01/01/21'
 SET @End = '01/01/22'
 
---cte_purpose
-; with uc as 
-
-       (
-       select *
-       from Adm_Orders_DC_Import
-       where ORDER_MNEMONIC like '%urine culture%'
-       )
-
 --cte_purpose: create dummy variables for source, including 'CleanCatch', 'Cath', 'Bagged'
-, ucx_source as
+; with ucx_source as
 	(
-	select pt_fin
+	select pt_fin as pt_fin_ucx_source
 	, orig_order_dt_tm
+	, CATALOG_TYPE
     , case when order_detail like '%clean%' then 'CleanCatch'
       when order_detail like '%cath%' Then 'Cath'
       when ORDER_DETAIL like '%bag%' then 'Bagged'
       else order_detail
       end Source
-	from uc
+  from Adm_Orders_DC_Import
+  where ORDER_MNEMONIC like '%urine culture%' and CATALOG_TYPE like '%lab%'
+--and ORIG_ORDER_DT_TM between @start and @End
 	)
 
 select *
