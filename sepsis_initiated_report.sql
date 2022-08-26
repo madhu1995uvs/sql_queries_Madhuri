f38a7c5234d6e9b8e88917dc1df43fa757070bd2
kennymckinley <56144157+kennymckinley@users.noreply.github.com>
2022-08-26 14:42:40 -0400

    create sepsis report

---
+++ sepsis_initiated_report.sql
@@ -0,0 +1,34 @@
+; with ct as
+	(
+	select *
+	from COVID_TAT
+	where CHECKIN_DATE_TIME between '01/01/2022' and '08/26/2022'
+	)
+
+--cte_purpose: capture patients with sepsis pathway initiated
+, seps as
+	(
+	select fin, POTENTIAL_SEPSIS_ORDER_DATE
+	--, format(potential_sepsis_order_date, 'yyyy-MM-dd') as seps_date
+	--, left(potential_sepsis_order_date, 11) as seps_date
+	, SEPSIS_PATHWAY_INITIATED_ORDERED, SEPSIS_PATHWAY_INITIATED_ORDER_STATUS
+	from sepsis_order_import
+	)
+
+--cte_purpose: review the column names from the sepsis_order_import table
+, seps_cols as
+	(
+	select column_name, ordinal_position, data_type
+	from INFORMATION_SCHEMA.columns
+	where table_name = 'sepsis_order_import'
+	)
+
+--cte_purpose: create variables for fin, mrn, track_group, age_months, to join with sepsis data
+, tat as
+	(
+	select patient_fin, patient_mrn, track_group, checkin_date_time, datediff(month,pt_dob,checkin_date_time) as age_months
+	from ED_TAT_MASTER
+	where checkin_date_time between '01/01/2022' and '08/27/2022'
+	)
+
+select * from tat inner join seps on tat.patient_fin = seps.fin
